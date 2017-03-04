052e83c87f0665c63a03db522fa21a0f6159216e
Rauan Assis <rauan.assis@outlook.com>
2017-03-04 15:51:24 -0300

    :art: add typed.js and finish page

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # contact.rauan
+My personal website.
---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function() {
+  $('#typed').typed({
+    strings: [
+    "Hi",
+    "I'm Rauan Assis",
+    "Welcome."
+    ],
+    typeSpeed: 60,
+    backSpeed: 30,
+    backDelay: 1400,
+    loop: !0,
+    startDelay: 1000,
+  });
+  $('a').click( function() {
+    $(this).toggleClass("dont-turn-blue");
+  });
+}); 
